f4d9f8370260efdccb844d2112077dc0b9409918
MrBroooo <broooooomr@gmail.com>
2022-01-18 23:35:04 +0500

    IN PROGRESS!

--- src/main/java/byfayzullayev/jaluzi/service/UserService.java
+++ src/main/java/byfayzullayev/jaluzi/service/UserService.java
@@ -1,8 +1,24 @@
 package byfayzullayev.jaluzi.service;
 
+import byfayzullayev.jaluzi.repository.RoleRepository;
+import byfayzullayev.jaluzi.repository.UserRepository;
+import byfayzullayev.jaluzi.service.base.BaseService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements BaseService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    private final ObjectMapper objectMapper;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository, ObjectMapper objectMapper) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+        this.objectMapper = objectMapper;
+    }
 }
