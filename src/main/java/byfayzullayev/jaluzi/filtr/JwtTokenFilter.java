82705aaae56b04f38f2b82fab86a63ecc3666d80
MrBroooo <broooooomr@gmail.com>
2022-01-31 18:32:43 +0500

    IN PROGRESS!

--- src/main/java/byfayzullayev/jaluzi/filtr/JwtTokenFilter.java
+++ src/main/java/byfayzullayev/jaluzi/filtr/JwtTokenFilter.java
@@ -1,8 +1,32 @@
 package byfayzullayev.jaluzi.filtr;
 
+import byfayzullayev.jaluzi.service.ApplicationUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
-public class JwtTokenFilter {
+public class JwtTokenFilter extends OncePerRequestFilter {
 
+    private final ApplicationUserDetailService applicationUserDetailService;
+
+    @Autowired
+    public JwtTokenFilter(ApplicationUserDetailService applicationUserDetailService) {
+        this.applicationUserDetailService = applicationUserDetailService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
 }
