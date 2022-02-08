c112318ad37427e5888084c3d223dea1716764af
MrBroooo <broooooomr@gmail.com>
2022-02-08 10:51:22 +0500

    IN PROGRESS!

--- src/main/java/byfayzullayev/jaluzi/controller/UserController.java
+++ src/main/java/byfayzullayev/jaluzi/controller/UserController.java
@@ -1,5 +1,11 @@
 package byfayzullayev.jaluzi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/yourjaluzi/user")
 public class UserController {
+
+
 }
